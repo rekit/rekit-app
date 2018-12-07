ea737df4d30dda521aa49b3a8551c2c12a7a52e8
Guillaume Hain <zedtux@zedroot.org>
2018-12-07 16:11:01 +0100

    Migrates from react-router-redux to connected-react-router react-router-redux is deprecated and connected-react-router recommended as a replacement

--- src/common/configStore.js
+++ src/common/configStore.js
@@ -1,44 +1,42 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { routerMiddleware } from 'react-router-redux';
-import history from './history';
-import rootReducer from './rootReducer';
-
-const router = routerMiddleware(history);
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { routerMiddleware } from 'connected-react-router'
+import history from './history'
+import createRootReducer from './rootReducer'
 
 // NOTE: Do not change middleares delaration pattern since rekit plugins may register middlewares to it.
 const middlewares = [
   thunk,
-  router,
-];
+  routerMiddleware(history),
+]
 
-let devToolsExtension = f => f;
+let devToolsExtension = f => f
 
 /* istanbul ignore if  */
 if (process.env.NODE_ENV === 'development') {
-  const { createLogger } = require('redux-logger');
+  const { createLogger } = require('redux-logger')
 
-  const logger = createLogger({ collapsed: true });
-  middlewares.push(logger);
+  const logger = createLogger({ collapsed: true })
+  middlewares.push(logger)
 
   if (window.devToolsExtension) {
-    devToolsExtension = window.devToolsExtension();
+    devToolsExtension = window.devToolsExtension()
   }
 }
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState, compose(
+  const store = createStore(createRootReducer(history), initialState, compose(
     applyMiddleware(...middlewares),
     devToolsExtension
-  ));
+  ))
 
   /* istanbul ignore if  */
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('./rootReducer', () => {
-      const nextRootReducer = require('./rootReducer').default; // eslint-disable-line
-      store.replaceReducer(nextRootReducer);
-    });
+      const nextRootReducer = require('./rootReducer').default // eslint-disable-line
+      store.replaceReducer(nextRootReducer)
+    })
   }
-  return store;
+  return store
 }
--- src/common/rootReducer.js
+++ src/common/rootReducer.js
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import homeReducer from '../features/home/redux/reducer';
-import commonReducer from '../features/common/redux/reducer';
-import examplesReducer from '../features/examples/redux/reducer';
-import newProjectReducer from '../features/new-project/redux/reducer';
+import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+import homeReducer from '../features/home/redux/reducer'
+import commonReducer from '../features/common/redux/reducer'
+import examplesReducer from '../features/examples/redux/reducer'
+import newProjectReducer from '../features/new-project/redux/reducer'
 
 // NOTE 1: DO NOT CHANGE the 'reducerMap' name and the declaration pattern.
 // This is used for Rekit cmds to register new features, remove features, etc.
@@ -11,11 +11,13 @@ import newProjectReducer from '../features/new-project/redux/reducer';
 // So that it's easy for others to understand it and Rekit could manage them.
 
 const reducerMap = {
-  router: routerReducer,
   home: homeReducer,
   common: commonReducer,
   examples: examplesReducer,
   newProject: newProjectReducer,
-};
+}
 
-export default combineReducers(reducerMap);
+export default (history) => combineReducers({
+  router: connectRouter(history),
+  ...reducerMap
+})
