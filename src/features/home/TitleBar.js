045f2ba61bfa765607013cd95099e550aa6bfb51
Nate Wang <supnate@gmail.com>
2019-04-29 00:09:16 +0800

    Hide caret-down icon when no studios open.

--- src/features/home/TitleBar.js
+++ src/features/home/TitleBar.js
@@ -62,7 +62,7 @@ export class TitleBar extends Component {
       <header className="home-title-bar" onDoubleClick={this.handleDoubleClick}>
         <span className="title-container">
           <span className="project-name">{current.prjDir.split(/\\|\//).pop()}</span>
-          <Icon type="caret-down" />
+          {studios.length > 0 && <Icon type="caret-down" />}
           {studios.length > 0 && (
             <div className={`project-list ${this.state.hideDropdown ? 'hide-dropdown' : ''}`}>
               <ul>
