ded37522bc92dd8f9803654a592a23ba03a4deca
Nate Wang <supnate@gmail.com>
2019-06-09 08:55:41 +0800

    Make 404 page empty.

--- src/features/common/PageNotFound.js
+++ src/features/common/PageNotFound.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 
 export default class PageNotFound extends PureComponent {
   render() {
-    return (
-      <div className="common-page-not-found">
-        Ops, page not found.
-        <p>
-          <Link to="/">Back to welcome page.</Link>
-        </p>
-      </div>
-    );
+    return null;
   }
+  // render() {
+  //   return (
+  //     <div className="common-page-not-found">
+  //       Ops, page not found.
+  //       <p>
+  //         <Link to="/">Back to welcome page.</Link>
+  //       </p>
+  //     </div>
+  //   );
+  // }
 }
