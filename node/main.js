b6ae1516926d8e824052b8fb3824f0c0a2dff0e4
Nate Wang <supnate@gmail.com>
2019-07-19 23:08:44 +0800

    Code clean

--- node/main.js
+++ node/main.js
@@ -9,7 +9,6 @@ const menu = require('./menu');
 
 const path = require('path');
 const taskRunner = require('./taskRunner');
-// const init = require('./init');
 require('./ua');
 
 require('./mainService');
